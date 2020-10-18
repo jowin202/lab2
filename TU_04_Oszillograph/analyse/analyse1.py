4d250d6c3811c38da4b3a5f88b9dfa6f43fe3e53
jowin202 <johannes.w@gmx.at>
2020-10-18 12:57:50 +0200

    TU_04 le awesome shit

---
+++ TU_04_Oszillograph/analyse/analyse1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = open("data1.csv", "r")
+
+data = f.read()
+
+lines = data.split("\n")
+
+msecs = []
+channel1 = []
+
+
+for line in lines:
+    parts = line.split(",")
+    
+    if line != "":
+        msecs.append(float(parts[1]))
+        channel1.append(float(parts[2]))
+        #print(str(msecs[-1]) + " " + str(channel1[-1]))
+
+f.close()
+
+
+
+plt.plot(msecs, channel1)
+plt.savefig('channel1.png')
+
+
+
+def find_max(msecs, data, time_from, time_to):
+    current_max = -100
+    current_max_time = -100
+    for i in range(len(msecs)):
+        if msecs[i] >= time_from/1000.0 and msecs[i] <= time_to/1000.0 :
+            if data[i] > current_max:
+                current_max = data[i]
+                current_max_time = msecs[i]
+    return [current_max_time, current_max]
+
+
+print(find_max(msecs, channel1, -20, -15))
+print(find_max(msecs, channel1, 0, 5))
+print(find_max(msecs, channel1, 20, 25))
+
+
+
