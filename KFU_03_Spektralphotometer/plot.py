3a3d70de416cc567176b4aa7295b4e8354197f01
jowin202 <johannes.w@gmx.at>
2020-11-15 15:38:58 +0100

    KFU_03

--- KFU_03_Spektralphotometer/plot.py
+++ KFU_03_Spektralphotometer/plot.py
@@ -117,6 +117,22 @@ def log_of_a_list(list1):
 
 
 
+def calc_k(list1, list2):
+    if len(list1) != len(list2):
+        print("weird error 2")
+        exit(1)
+    sum_xy = 0
+    sum_x2 = 0
+    sum_x = 0
+    sum_y = 0
+    length = len(list1)
+    for i in range(len(list1)):
+        sum_xy += list1[i]*list2[i]
+        sum_x2 += list1[i]*list1[i]
+        sum_x += list1[i]
+        sum_y += list2[i]
+    return (sum_xy - (sum_x*sum_y)/length)/(sum_x2 - sum_x**2/length)
+
 def filter664(list1, list2):
     l1 = -1
     l2 = -1
@@ -136,6 +152,38 @@ def filter664(list1, list2):
     return E1 + k*(664-l1)
     
 
+
+def find_max(list1,list2):
+    maximum = -1
+    max_i = -1
+    if len(list1) != len(list2):
+        print("weird error 3")
+        exit(1)
+    for i in range(len(list1)):
+        if list2[i] > maximum:
+            max_i = i
+            maximum = list2[i]
+    return list1[max_i], maximum
+
+
+def find_transmission_max(list1,list2):
+    if len(list1) != len(list2):
+        print("weird error 3")
+        exit(1)
+    
+    max_lambda_list = []
+    max_transmission_list = []
+    for i in range(len(list1)):
+        if list1[i] > 700 and list1[i] < 720:
+            if list2[i] > list2[i-1] and list2[i] > list2[i-2]  and  list2[i] > list2[i-3]  :
+                if list2[i] > list2[i+1] and list2[i] > list2[i+2]  and  list2[i] > list2[i+3]:
+                    max_lambda_list.append(list1[i])
+                    max_transmission_list.append(list2[i])
+    return max_lambda_list, max_transmission_list
+                    
+            
+            
+    
 #referenzspektrum
 refspec_lambda, refspec_I = read_csv("daten/1/I0.csv")
 
@@ -175,9 +223,68 @@ print("c: " + str( c ) )
 print("Delta c: " + str(  Delta_c ))
 
 f = open("methyl_ergebnis.tex", "w")
-f.write("\\begin{align*}\n\\\\")
-f.write("c = (" + str(custom_round(c*10**6,2)) + " \pm " +  str(custom_round(Delta_c*10**6,2)) + ")\cdot 10^6~\\text{mol/Liter}\n\\\\" )
-f.write("\\end{align*}\n\\\\")
+f.write("\\begin{align*}\n")
+f.write("c = (" + str(custom_round(c*10**6,2)) + " \pm " +  str(custom_round(Delta_c*10**6,2)) + ")\\cdot 10^6~\\text{mol/Liter} \n" )
+f.write("\\end{align*}\n")
+
+
+
+
+
+
+#Glasplatte
+glas_lambda, glas_ref_I = read_one_color(["daten/4/I0.csv"])
+glas_lambda, glas_I = read_one_color(["daten/4/I Glas 1.csv","daten/4/I Glas 2.csv","daten/4/I Glas 3.csv","daten/4/I Glas 4.csv"])
+
+transmission_glas = divide_two_lists(glas_I,glas_ref_I)
+extinktion_glas = log_of_a_list(divide_two_lists(glas_ref_I,glas_I))
+
+glas_max_lambda, glas_max_I = find_max(glas_lambda, glas_I)
+
+f = open("glas_max.tex", "w")
+f.write("\\begin{align*}\n")
+f.write("I_{\\text{max}} = " + str(custom_round(glas_max_I,2)) + "\\qquad \\text{bei} \qquad \\lambda = " + str(custom_round(glas_max_lambda,2)) + " \n")
+f.write("\\end{align*}\n")
+
+glas_plot_limit = (10*round(glas_max_lambda/10))-10
+
+
+
+transmission_max_lambda_glas,transmission_max_glas = find_transmission_max(glas_lambda, transmission_glas)
+f = open("glas_max_trans_tab.tex", "w")
+f.write("\\begin{tabular}{cccc}\n")
+f.write("m & $\\lambda_m$ / nm & $\\nu_m$ / $\mu$m${}^{-1}$ & T / 1 \\\\\n\\hline")
+count = 1
+m_list = []
+lambda_inv_list = []
+for i in range(len(transmission_max_lambda_glas)):
+    m_list.append(i+1)
+    lambda_inv_list.append(1/transmission_max_lambda_glas[i])
+    f.write(str(count) + " & " + str(custom_round(transmission_max_lambda_glas[i],1)) + " & " + str(custom_round(1000/transmission_max_lambda_glas[i],3)) + " & " + str(custom_round(transmission_max_glas[i],3)) + "\\\\\n")
+    count += 1
+f.write("\\end{tabular}\n")
+
+glas_k = calc_k(m_list, lambda_inv_list)
+
+f = open("glas_k_ergebnis.tex", "w")
+f.write("\\begin{align*}\n")
+f.write("k = (" + str(custom_round(10**6*glas_k,2)) + " \pm 0.08)\\cdot 10^{-6}~\\text{nm}^{-1} \n" )
+f.write("\\end{align*}\n")
+
+
+
+np = 1.519
+Delta_np = 0.001
+Delta_k = 0.08*10**-6
+d = -1/(2 * np * glas_k) #minus machen
+delta_d = d/np * Delta_np - d/glas_k * Delta_k
+f = open("glas_d_ergebnis.tex", "w")
+f.write("\\begin{align*}\n")
+f.write("d = (" + str(custom_round(d*10**-6,2)) + " \pm " + str(custom_round(10**-6*delta_d,2)) +  ")~\\text{mm} \n" )
+f.write("\\end{align*}\n")
+
+
+
 
 fig, ax = plt.subplots() 
 ax.axis([400, 800, 0, 1])
@@ -275,7 +382,7 @@ ax1.axis([400, 800, 0.6, 1])
 ax1.plot(methylblau_lambda, transmission_methyl, 'blue')
 ax1.legend(['Transmission'])
 ax1.set_ylabel('T / 1')
-ax1.set_title('Extinktion / Transmission Methylenblau')
+ax1.set_title('Transmission / Extinktion Methylenblau')
 #plt.savefig("Transmission_Methyl.png")
 
 ax2.axis([400, 800, 0, 0.3])
@@ -288,4 +395,34 @@ plt.savefig("Transmission_Extinktion_Methyl.png")
 
 
 
+
+#glas
+
+fig, ax = plt.subplots() 
+ax.axis([400, 800, 0, 1])
+ax.plot(glas_lambda, glas_ref_I, 'gray')
+ax.plot(glas_lambda, glas_I, 'red')
+ax.legend(['Referenzspektrum (kein Glas)', 'Glas'])
+ax.set_xlabel('lambda / nm')
+ax.set_ylabel('I / 1')
+ax.set_title('Intensitäten mit und ohne Glasplatte')
+plt.savefig("Glas_Intensitaeten.png")
+
+
+fig, (ax1,ax2) = plt.subplots(2,1) 
+ax1.axis([glas_plot_limit, glas_plot_limit+20, 0.89, 0.92])
+ax1.plot(glas_lambda, transmission_glas, 'blue')
+ax1.legend(['Transmission Extinktion der Glasplatte'])
+ax1.set_ylabel('T / 1')
+ax1.set_title('Transmission / Extinktion Glasplatte')
+for m in transmission_max_lambda_glas:
+    ax1.axvline(x=m)
+
+
+ax2.axis([glas_plot_limit, glas_plot_limit+20, 0.04, 0.05])
+ax2.plot(glas_lambda, extinktion_glas, 'red')
+ax2.legend(['Extinktion der Glasplatte'])
+ax2.set_xlabel('lambda / nm')
+ax2.set_ylabel('E / 1')
+plt.savefig("Glas_Transmission_Extinktion.png")
 
